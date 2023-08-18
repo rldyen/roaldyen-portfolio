43802dc727722335b824f7a218adb857da3a1ad7
Roald Yen <roald112100@gmail.com>
2023-08-18 13:14:36 +0800

    Bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
