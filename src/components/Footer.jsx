9b402ef5717ee3ab5d36d8ee112ab274397e6972
Roald Yen <roald112100@gmail.com>
2025-09-17 15:35:37 +0800

    changed year to dynamic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default function Footer() {
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <div>
-        <p>© rldyen. 2023. All rights reserved.</p>
+        <p>© rldyen. {currentYear}. All rights reserved.</p>
       </div>
     </footer>
   );
